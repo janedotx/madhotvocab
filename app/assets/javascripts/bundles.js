a111a7e9fd572f5deda7ae545f2cd56afeff1374
jian <jane.x.huang@gmail.com>
2013-09-05 20:19:33 -0700

    js file for making drag/drop work

--- app/assets/javascripts/bundles.js
+++ app/assets/javascripts/bundles.js
@@ -1,5 +1,40 @@
-
 $(function() {
-  $(".answer").draggable();
+  $(".analogies-answer").draggable({ snap: ".answer-slot, .answer-home", snapMode: "inner", 
+                                     revert: function(event, ui) {
+                                               $(this).data("uiDraggable").originalPosition = {
+                                                 top: 0,
+                                                 left: 0
+                                               }
 
-  });
+                                              return !event;
+
+                                     }
+                                     
+                                  });
+
+  var droppableClassStrings = ["#root_target", "#second_source", "#second_target"];
+  for (var i = 0; i < droppableClassStrings.length; i++) {
+    $(droppableClassStrings[i]).droppable({
+       drop: function( event, ui ) {
+              var $slot = $(this);
+              $(this).droppable('option', 'accept', ui.draggable);
+              if ($(ui.draggable).is(("#" + $slot.attr('id') + "_answer"))) {
+                $slot.addClass( "correct" )
+                }
+            },
+        out: function(event, ui) {
+          $(this).removeClass("correct");
+          $(this).droppable("option", 'accept', '.analogies-answer');
+        }
+         
+    });
+
+    $(".answer-home").droppable({
+      accept: ".analogies-answer"
+    });
+  }
+});
+
+$(document).ready(function() {
+  
+});
